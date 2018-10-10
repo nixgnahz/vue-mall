7ca9472fe9e394e7c7b9d50f9c6bab6a75ad5b63
nixgnahz <surezhangxin@163.com>
2018-10-10 17:51:10 +0800

    增加优惠券

--- src/components/Ticket/Index.vue
+++ src/components/Ticket/Index.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        Ticket
-    </div>
+    <section class='ticket'>
+        <p class='expire'>您有<span>{{expire}}</span>张优惠券即将过期</p>
+        <TicketItem v-for='item in ticketArr' :item='item' :key='item.id'/>
+        <p class='over'>没有更多了～</p>
+    </section>
 </template>
+
+<script>
+    import TicketItem from './TicketItem.vue'
+    export default {
+        components: {
+            TicketItem
+        },
+        data () {
+            return {
+                expire: 1,
+                ticketArr: [
+                    {
+                        id: 1,
+                        money: 5,
+                        condition: '满25可用',
+                        name: '电子产品抵扣券',
+                        tip: '全场电子产品通用',
+                        startTime: '2018-09-01',
+                        endTime: '2018-10-12'
+                    },
+                    {
+                        id: 2,
+                        money: 8,
+                        condition: '满35可用',
+                        name: '零食抵扣券',
+                        tip: '部分零食可以',
+                        startTime: '2018-10-09',
+                        endTime: '2018-10-19'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import './index.scss';
+</style>
---
+++ src/components/Ticket/TicketItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class='ticket-item'>
+        <div class='ticket-item-money'>
+            <img src='../../assets/image/icon/ticket.png'/>
+            <div class='content'>
+                <p class='money'>¥<span>{{item.money}}</span></p>
+                <p class='condition'>{{item.condition}}</p>
+            </div>
+        </div>
+        <div class='ticket-item-desc'>
+            <img src='../../assets/image/icon/ticket-box.png'/>
+            <div class='content'>
+                <p class='name'>{{item.name}}</p>
+                <p class='time'>{{item.startTime}} - {{item.endTime}}</p>
+                <p class='tip'>{{item.tip}}</p>
+                <div class='use-btn'>
+                    <p>立即使用</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['item']
+    }
+</script>
